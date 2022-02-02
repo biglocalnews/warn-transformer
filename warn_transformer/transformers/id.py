a7c0e84329b5832939f9286a155bc4fd2f696a07
palewire <b@palewi.re>
2022-02-02 11:59:24 -0800

    Added ID transformer

---
+++ warn_transformer/transformers/id.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform Idaho raw data for consolidation."""
+
+    postal_code = "ID"
+    fields = dict(
+        company="Company",
+        location="City",
+        date="Date of Letter",
+        jobs="No. of Employees Affected",
+    )
+    date_format = "%m/%d/%Y"
+    date_corrections = {
+        "2/19/219": datetime(2019, 2, 19),
+    }
+
+    def transform_date(self, value: str) -> str:
+        """Transform a raw date string into a date object.
+
+        Args:
+            value (str): The raw date string provided by the source
+
+        Returns: A date object ready for consolidation. Or, if the date string is invalid, a None.
+        """
+        # A little custom clean up based on the weird stuff from this source
+        value = value.strip().split()[0].replace(",", "").strip()
+
+        # The same old stuff
+        return super().transform_date(value)
