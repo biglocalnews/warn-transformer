c5186c43d14b09a2ce0e4fa4dff7f1c42637b382
palewire <b@palewi.re>
2022-02-02 11:48:20 -0800

    Added DE transformer

---
+++ warn_analysis/transformers/de.py
@@ -0,0 +1,14 @@
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform Deleware raw data for consolidation."""
+
+    postal_code = "DE"
+    fields = dict(
+        company="employer",
+        location="city",
+        date="notice_date",
+        jobs="number_of_employees_affected",
+    )
+    date_format = "%b %d, %Y"
