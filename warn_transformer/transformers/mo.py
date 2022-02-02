9b37b646693454d7e703ef8fccc8c4f87e2a2da0
palewire <b@palewi.re>
2022-02-02 12:42:16 -0800

    Added MO transformer

---
+++ warn_transformer/transformers/mo.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform Missouri raw data for consolidation."""
+
+    postal_code = "MO"
+    fields = dict(
+        company="COMPANY NAME",
+        location="LOCATION",
+        date="DATE RECEIVED",
+        jobs="# AFFECTED",
+    )
+    date_format = "%m/%d/%Y"
+
+    def transform_date(self, value: str) -> str:
+        """Transform a raw date string into a date object.
+
+        Args:
+            value (str): The raw date string provided by the source
+
+        Returns: A date object ready for consolidation. Or, if the date string is invalid, a None.
+        """
+        # A little custom clean up based on the weird stuff from this source
+        value = value.strip().split()[0].strip()
+
+        try:
+            dt = datetime.strptime(value.strip(), "%m/%d/%y")
+        except ValueError:
+            return super().transform_date(value)
+        return str(dt.date())
