01c8f9deec98620113e0c2b090d952eaefbed130
palewire <b@palewi.re>
2022-02-01 16:09:01 -0800

    Broke out a BaseTransformer from IA

--- src/schema.py
+++ src/schema.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from marshmallow import Schema, fields
 
 
@@ -8,3 +10,66 @@ class WarnNoticeSchema(Schema):
     company = fields.Str(required=True)
     date = fields.Date(allow_none=True)
     jobs = fields.Int(required=True, allow_none=True)
+
+
+class BaseTransformer:
+    """Transform a state's raw data for consolidation."""
+
+    date_format = (
+        "%m/%d/%Y"  # The default date format. It will need to be customized by source.
+    )
+
+    def __init__(self, row_list):
+        self.row_list = self.prep_row_list(row_list)
+
+    def transform(self):
+        """Transform prepared rows into a form that's ready for consolidation."""
+        return [self.transform_row(r) for r in self.row_list]
+
+    def prep_row_list(self, row_list):
+        """Make necessary transformations to the raw row list prior to transformation.
+
+        Useful for filtering out empty rows.
+        """
+        # By default it does nothing.
+        # Override in your subclass if you need to do something.
+        return row_list
+
+    def transform_row(self, row):
+        """Transform a row into a form that's ready for consolidation.
+
+        Args:
+            row (dict): One raw row of data from the source
+
+        Returns: A transformed dict that's ready to be loaded into our consolidated schema.
+        """
+        raise NotImplementedError("transform_row method not implemented")
+
+    def transform_date(self, value):
+        """Transforms a raw date string into a date object.
+
+        Args:
+            value (str): The raw date string provided by the source
+
+        Returns: A date object ready for consolidation. Or, if the date string is invalid, a None.
+        """
+        value = value.strip()
+        try:
+            dt = datetime.strptime(value, self.date_format)
+        except ValueError:
+            return None
+        return str(dt.date())
+
+    def transform_jobs(self, value):
+        """Transforms a raw jobs number into an integer.
+
+        Args:
+            value (str): A raw jobs number provided by the source
+
+        Returns: An integer number ready for consolidation. Or, if the value is invalid, a None.
+        """
+        value = value.strip()
+        try:
+            return int(value)
+        except ValueError:
+            return None
--- src/transformers/ia.py
+++ src/transformers/ia.py
@@ -1,17 +1,8 @@
-from datetime import datetime
+from ..schema import BaseTransformer
 
 
-class Transformer:
-    """Transform a state's raw data for consolidation."""
-
+class Transformer(BaseTransformer):
     date_format = "%m/%d/%Y"
-
-    def __init__(self, row_list):
-        self.row_list = self.prep_row_list(row_list)
-
-    def transform(self):
-        """Transform prepared rows into a form that's ready for consolidation."""
-        return [self.transform_row(r) for r in self.row_list]
 
     def prep_row_list(self, row_list):
         """Make necessary transformations to the raw row list prior to transformation.
@@ -45,31 +36,3 @@ class Transformer:
             date=self.transform_date(row["Notice Date"]),
             jobs=self.transform_jobs(row["Emp #"]),
         )
-
-    def transform_date(self, value):
-        """Transforms a raw date string into a date object.
-
-        Args:
-            value (str): The raw date string provided by the source
-
-        Returns: A date object ready for consolidation. Or, if the date string is invalid, a None.
-        """
-        value = value.strip()
-        try:
-            dt = datetime.strptime(value, self.date_format)
-        except ValueError:
-            return None
-        return str(dt.date())
-
-    def transform_jobs(self, value):
-        """Transforms a raw jobs number into an integer.
-
-        Args:
-            value (str): A raw jobs number provided by the source
-
-        Returns: An integer number ready for consolidation. Or, if the value is invalid, a None.
-        """
-        try:
-            return int(value)
-        except ValueError:
-            return None
