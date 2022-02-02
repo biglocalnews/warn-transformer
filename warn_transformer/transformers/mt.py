55d4a86aef61cf51c6cbc8332144bffb712fb85c
palewire <b@palewi.re>
2022-02-02 12:44:46 -0800

    Added MO transformer

---
+++ warn_transformer/transformers/mt.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform Montana raw data for consolidation."""
+
+    postal_code = "MT"
+    fields = dict(
+        company="Name of Company",
+        location="County",
+        date="Date of Notice",
+        jobs="Number of Employees Affected",
+    )
+    date_format = "%Y-%m-%d"
