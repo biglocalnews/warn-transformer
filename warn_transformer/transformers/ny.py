324d5d9a0712b54884c16cfcf6cb14124982b320
palewire <b@palewi.re>
2022-02-02 13:24:20 -0800

    Added NY transformer

---
+++ warn_transformer/transformers/ny.py
@@ -0,0 +1,14 @@
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform New York raw data for consolidation."""
+
+    postal_code = "NY"
+    fields = dict(
+        company="Company",
+        location="City",
+        date="Notice Date",
+        jobs="Number Affected",
+    )
+    date_format =  "%Y-%m-%d %H:%M:%S"
