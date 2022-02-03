ddcc168313be3ec752c8e3058d7ee44e5718242e
palewire <b@palewi.re>
2022-02-03 12:00:42 -0800

    Cut Colorado cruft

--- warn_transformer/transformers/co.py
+++ warn_transformer/transformers/co.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import typing
 
 from ..schema import BaseTransformer
 
@@ -26,27 +25,3 @@ class Transformer(BaseTransformer):
         ),
         "05/24/19 - 3/20/2020": datetime(2019, 5, 24),
     }
-
-    def prep_row_list(
-        self, row_list: typing.List[typing.Dict]
-    ) -> typing.List[typing.Dict]:
-        """Make necessary transformations to the raw row list prior to transformation.
-
-        Args:
-            row_list (list): A list of raw rows of data from the source.
-
-        Returns: The row list minus empty records
-        """
-        # Do the standard stuff
-        row_list = super().prep_row_list(row_list)
-
-        # Cut rows with only one value
-        prepped_list = []
-        for row in row_list:
-            # Skip rows with only one or two values
-            value_list = [v for v in row.values() if v.strip()]
-            if len(value_list) > 2:
-                prepped_list.append(row)
-
-        # Return the result
-        return prepped_list
