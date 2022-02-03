c82b55ac0b2e136f776248189169f9ebc013e360
palewire <b@palewi.re>
2022-02-02 17:26:28 -0800

    Added TX transformer

---
+++ warn_transformer/transformers/tx.py
@@ -0,0 +1,14 @@
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform Texas raw data for consolidation."""
+
+    postal_code = "TX"
+    fields = dict(
+        company="JOB_SITE_NAME",
+        location="CITY_NAME",
+        date="NOTICE_DATE",
+        jobs="TOTAL_LAYOFF_NUMBER",
+    )
+    date_format = "%Y-%m-%d"
