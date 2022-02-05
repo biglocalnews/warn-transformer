94becd53c874147a901d8a04ff4c2e870fc23031
palewire <b@palewi.re>
2022-02-05 06:54:48 -0800

    Fixing it all up

--- warn_transformer/transformers/ri.py
+++ warn_transformer/transformers/ri.py
@@ -6,9 +6,19 @@ class Transformer(BaseTransformer):
 
     postal_code = "RI"
     fields = dict(
-        company="Company Name",
-        location="Location Of Layoffs",
+        company="Company Name (* Denotes Covid 19 Related WARN)",
+        location="Location of Layoffs",
         date="WARN Date",
         jobs="Number Affected",
     )
-    date_format = ("%m/%d/%y", "%m/%d/%Y")
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    def transform_company(self, value: str) -> str:
+        """Transform a raw company name.
+
+        Args:
+            value (str): The raw company string provided by the source
+
+        Returns: A string object ready for consolidation.
+        """
+        return value.strip().replace("*", "")
