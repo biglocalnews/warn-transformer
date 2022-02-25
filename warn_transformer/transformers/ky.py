d735f0fe36a258aad99a9aaba6dc16fe247e10e5
palewire <b@palewi.re>
2022-02-24 16:48:16 -0800

    KY thing

--- warn_transformer/transformers/ky.py
+++ warn_transformer/transformers/ky.py
@@ -1,3 +1,6 @@
+import re
+import typing
+
 from ..schema import BaseTransformer
 
 
@@ -14,6 +17,35 @@ class Transformer(BaseTransformer):
         jobs="Employees",
     )
     date_format = "%Y-%m-%d %H:%M:%S"
+    minimum_year = 1997
     date_corrections = {
         "43490.0": None,
+        "N/A": None,
+        "November": None,
     }
+    jobs_corrections = {
+        "?": None,
+        "N/A": None,
+        "See W.A.R.N.": None,
+        "74 fulltime and 184 parttime": 74,
+        "Reduction from 13 to 1": 12,
+        "See WARN": None,
+        "in WARN": None,
+        "Unknown": None,
+        "TBD": None,
+    }
+
+    def transform_jobs(self, value: str) -> typing.Optional[int]:
+        """Transform a raw jobs number into an integer.
+
+        Args:
+            value (str): A raw jobs number provided by the source
+
+        Returns: An integer number ready for consolidation. Or, if the value is invalid, a None.
+        """
+        value = value.split("-")[0].strip()
+        value = value.replace("+/-", "")
+        value = value.replace("+/", "").strip()
+        value = value.replace("+", "").strip()
+        value = re.split(" {5,}", value)[0].strip()
+        return super().transform_jobs(value)
