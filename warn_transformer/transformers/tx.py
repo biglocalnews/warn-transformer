9f0c2cc2131520b9cc6794cc6e00c3117dfc2806
palewire <b@palewi.re>
2022-03-10 15:57:48 -0800

    TX

--- warn_transformer/transformers/tx.py
+++ warn_transformer/transformers/tx.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from ..schema import BaseTransformer
 
 
@@ -8,7 +10,13 @@ class Transformer(BaseTransformer):
     fields = dict(
         company="JOB_SITE_NAME",
         location="CITY_NAME",
-        date="NOTICE_DATE",
+        notice_date="NOTICE_DATE",
+        effective_date="LayOff_Date",
         jobs="TOTAL_LAYOFF_NUMBER",
     )
     date_format = "%Y-%m-%d %H:%M:%S"
+    date_corrections = {
+        "1930-03-30 00:00:00": datetime(2020, 3, 30),
+        "1930-03-31 00:00:00": datetime(2020, 3, 31),
+        "2027-03-01 00:00:00": datetime(2017, 3, 1),
+    }
