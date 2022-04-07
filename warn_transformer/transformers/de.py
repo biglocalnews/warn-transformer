daefc8835ed369e19838410d4c94950c43800d1e
Ben Welsh <b@palewi.re>
2022-04-06 18:17:02 -0700

    DE date fix (#108)
    * Handle NY scrape
    
    * Updated NY test file
    
    * Fix DE date parsing

--- warn_transformer/transformers/de.py
+++ warn_transformer/transformers/de.py
@@ -8,7 +8,7 @@ class Transformer(BaseTransformer):
     fields = dict(
         company="employer",
         location="city",
-        date="notice_date",
+        notice_date="notice_date",
         jobs="number_of_employees_affected",
     )
     date_format = "%b %d, %Y"
