c14e0bebf9520fad75336bfee758f5d8d2faaa49
Chris Zubak-Skees <chriszs@gmail.com>
2024-03-17 17:30:41 +0800

    Meet Great Expectations

--- warn_transformer/cli.py
+++ warn_transformer/cli.py
@@ -8,6 +8,7 @@ from . import consolidate as consolidate_runner
 from . import download as download_runner
 from . import integrate as integrate_runner
 from . import utils
+from . import validate as validate_runner
 
 
 @click.group()
@@ -107,6 +108,37 @@ def integrate(input_dir: Path, init: bool = False, log_level: str = "INFO"):
     logger = logging.getLogger(__name__)
     logger.debug("Running integrate command")
     integrate_runner.run(input_dir, init_current_data=init)
+
+
+@cli.command()
+@click.option(
+    "--input-dir",
+    default=utils.WARN_TRANSFORMER_OUTPUT_DIR / "raw",
+    type=click.Path(),
+    help="The Path where data to validate is located",
+)
+@click.option(
+    "--stage",
+    default="raw",
+    type=click.Choice(("raw",)),
+    help="The stage of the data to validate.",
+)
+@click.option(
+    "--log-level",
+    "-l",
+    default="INFO",
+    type=click.Choice(
+        ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"), case_sensitive=False
+    ),
+    help="Set the logging level",
+)
+def validate(input_dir: Path, stage: str = "raw", log_level: str = "INFO"):
+    """Validate the specified data"""
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.basicConfig(level=log_level, format="%(asctime)s - %(name)s - %(message)s")
+    logger = logging.getLogger(__name__)
+    logger.debug("Running validate command")
+    validate_runner.run(input_dir, stage)
 
 
 if __name__ == "__main__":
