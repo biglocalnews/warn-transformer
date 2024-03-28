567badd52ed0aa626abb323ec3fbbd3e8dda111d
Mike Stucka <stucka@whitedoggies.com>
2024-03-28 05:35:56 -0400

    Patch LA x2

--- warn_transformer/transformers/la.py
+++ warn_transformer/transformers/la.py
@@ -66,6 +66,8 @@ class Transformer(BaseTransformer):
         "38*": 38,
         "*292 *Only forty (40) employees affected in Louisiana.": 40,
         "*508 *Only sixty- three (63) employees affected in Louisiana.": 63,
+        "*179 *Some or all of the workers may be retained by the new vendor.": 0,
+        "*434 *Some or all may be picked up by new vendor": 0,
     }
 
     def transform_date(self, value: str) -> typing.Optional[str]:
