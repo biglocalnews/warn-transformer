82465ee626e0dc54b75d170216e6fe93a004f8b8
Mike Stucka <stucka@whitedoggies.com>
2025-12-23 20:49:45 -0500

    Incremental work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Configure the package for distribution."""
 import os
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def read(file_name):
@@ -45,39 +45,6 @@ def local_version(version):
 
 
 setup(
-    name="warn-transformer",
-    description="Consolidate, enrich and analyze the data gathered by warn-scraper",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Big Local News",
-    url="https://github.com/biglocalnews/warn-transformer",
-    packages=find_packages(
-        exclude=[
-            "tests",
-        ]
-    ),
-    include_package_data=True,
-    install_requires=[
-        "bln>=2.3.11",
-        "marshmallow==3.26.1",
-        "click",
-        "jellyfish",
-    ],
-    license="Apache 2.0 license",
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
     setup_requires=["setuptools_scm"],
     use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
     entry_points="""
